e21a2700c22a1a60e5e6bf60e8225c850d2d1ebb
Mateusz Glapiak <mat815@gmail.com>
2013-10-26 16:05:48 +0100

    Added Core Bluetoorh

---
+++ iOS/My Miles & More/BeaconManager.h
@@ -0,0 +1,13 @@
+//
+//  BeaconManager.h
+//  My Miles & More
+//
+//  Created by Mateusz Glapiak on 10/26/13.
+//  Copyright (c) 2013 Mateusz Glapiak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BeaconManager : NSObject
+
+@end
