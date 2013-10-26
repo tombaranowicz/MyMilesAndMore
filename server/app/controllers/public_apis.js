1ed0d104386f82bf690c605589331b4518422784
TomekB <tbaranowicz@gmail.com>
2013-10-27 00:43:36 +0200

    Server devices push in progress

--- server/app/controllers/public_apis.js
+++ server/app/controllers/public_apis.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose')
   , TagObject = mongoose.model('TagObject')
+  , GCM = require('gcm').GCM;
 
 exports.get_tag_details = function(req, res) { //GET// params tag_id
   console.log('get tag: '+req.params.tag_id);
@@ -8,8 +9,13 @@ exports.get_tag_details = function(req, res) { //GET// params tag_id
       console.log('error ' +err);
       return res.send(404);
     } else if (!object) {
-      console.log('no object');
-     return res.send(404);
+      TagObject.findOne({'uuid': req.params.tag_id}, function(err, object) {
+        if (err || !object){
+          return res.send(404);
+        } else {
+          res.send({'object':object});
+        }
+      });
     } else {
       res.send({'object':object});
     }
@@ -51,4 +57,56 @@ exports.delete_tag = function(req, res) { //POST// params tag_id
       });
     }
   });
+}
+
+exports.send_push = function(req, res) {  //POST// params tag_id, title, description 
+  TagObject.findOne({'tag_id' : req.body.tag_id}, function(err, object) {
+    if (err) return res.send({'error':err});
+    if (!object) {
+      return res.send({'error':'no object'});
+    } else {
+
+      var gcm = new GCM('AIzaSyCAGoHjZOwucv2QZbYHQKnAsCmbs5ty-oE');
+
+      for (var i=0;i<object.android_devices.length;i++)
+      { 
+        var device = devices[i];
+        console.log('found device ' + device);
+
+        if (gcm) {
+          var message = {
+            registration_id: device,
+            collapse_key: 'Collapse key', 
+            data:JSON.stringify({
+              'object': object,
+              'title': req.body.title,
+              'description': req.body.description,
+            })
+          };
+          gcm.send(message, function(err, messageId){
+            if (err) {
+              console.log("Something has gone wrong!");
+            } else {
+              console.log("Sent with message ID: ", messageId);
+            }
+          });
+        }
+      }
+    }
+  });
+}
+
+exports.add_android_device = function(req, res) {  //POST// params tag_id, android_device_token
+  TagObject.findOne({'tag_id' : req.body.tag_id}, function(err, object) {
+    if (err) return res.send({'error':err});
+    if (!object) {
+      return res.send({'error':'no object'});
+    } else {
+      object.android_devices.addToSet(android_device_token);
+      object.save(function(err){
+        if (!err) {
+          return res.send('ok':android_device_token);
+        }
+      });
+    }
 }
