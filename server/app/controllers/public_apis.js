b5d9418d44ae193d8a26ce62eb55ade895615dea
TomekB <tbaranowicz@gmail.com>
2013-10-26 16:53:10 +0200

    Server basic api added
    app.get('/objects/get_tag_details', public_api.get_tag_details) //params tag_id
      app.post('/objects/add_tag', public_api.add_tag) //POST// params tag_id, name, description, link
      app.post('/objects/delete_tag', public_api.delete_tag) //POST// params tag_id

---
+++ server/app/controllers/public_apis.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose')
+  , TagObject = mongoose.model('TagObject')
+
+exports.get_tag_details = function(req, res) { //GET// params tag_id
+  console.log('get tag: '+req.params.tag_id);
+  TagObject.findOne({'tag_id': req.params.tag_id}, function(err, object) {
+    if (err){
+      console.log('error ' +err);
+      return res.send({'object':'not_miles_and_mores'});
+    } else if (!object) {
+      console.log('no object');
+     return res.send({'object':'not_miles_and_mores'});
+    } else {
+      res.send({'object':object});
+    }
+  }); 
+}
+
+exports.add_tag = function(req, res) { //POST// params tag_id, name, description, link
+  console.log('add ' + JSON.stringify(req.body));
+  TagObject.findOne({'tag_id' : req.body.tag_id}, function(err, object) {
+  if (err){
+      console.log('error ' +err);
+      return res.send({'error':err});
+    } else if (object) {
+      console.log('no object');
+      return res.send({'error':'object_exists'});
+    } else {
+      var tagObject = new TagObject(req.body);
+      tagObject.save(function (err) {
+        if (err) res.send({'error':err});
+        return res.send({'object':tagObject});
+      });
+    }
+  });
+}
+
+exports.delete_tag = function(req, res) { //POST// params tag_id
+  console.log('delete ' + JSON.stringify(req.body));
+  TagObject.findOne({'tag_id' : req.body.tag_id}, function(err, object) {
+    if (err){
+      console.log('error ' +err);
+      return res.send({'error':err});
+    } else if (!object) {
+      console.log('no object');
+      return res.send({'error':'no object'});
+    } else {
+      object.remove(function(err) {
+        if (err) res.send({'error':err});
+        res.send({'object':req.body.tag_id});
+      });
+    }
+  });
+}
---
+++ server/app/models/object.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+
+var TagObjectSchema = new Schema({ //tag_id, name, description, link
+  name: {type : String, trim : true, required: true},
+  description: {type : String, trim : true, required: true},
+  tag_id: {type : String, trim : true, required: true},
+  link: {type : String, trim : true, required: true}
+})
+
+mongoose.model('TagObject', TagObjectSchema)
