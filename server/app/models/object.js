bc0a00b5c54fe311629d23a3c6a816721f764bdf
TomekB <tbaranowicz@gmail.com>
2013-10-27 01:47:08 +0200

    Country & city added to shop details

--- server/app/models/object.js
+++ server/app/models/object.js
@@ -11,6 +11,8 @@ var TagObjectSchema = new Schema({ //tag_id, name, description, link
   opening_hours: {type : String},
   phone: {type : String},
   foursquare_id: {type : String},
+  country: {type : String},
+  city: {type : String},
   description: {type : String, trim : true, required: true},
   tag_id: {type : String, trim : true, required: true},
   uuid: {type : String, trim : true, required: true},
