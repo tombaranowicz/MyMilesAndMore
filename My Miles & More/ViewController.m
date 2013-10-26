ed2c75b17d4ae7103371712bef86100cc8913f1f
Mateusz Glapiak <mat815@gmail.com>
2013-10-26 14:00:22 +0100

    Created repo. Created project. Configured project.

---
+++ My Miles & More/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  My Miles & More
+//
+//  Created by Mateusz Glapiak on 10/26/13.
+//  Copyright (c) 2013 Mateusz Glapiak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
