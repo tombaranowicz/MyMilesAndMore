d1971c28d7d0a6eec48c9d7a503b1c9fd826a68e
Mateusz Glapiak <mat815@gmail.com>
2013-10-27 07:39:54 +0000

    changed first view controller

--- iOS/My Miles & More/ViewController.m
+++ iOS/My Miles & More/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "NearbyStoresViewController.h"
+#import "UIImageView+AFNetworking.h"
 const CGFloat duration = 10.0;
 
 @interface ViewController ()
@@ -24,45 +25,9 @@ const CGFloat duration = 10.0;
     NearbyStoresViewController *vc = [[NearbyStoresViewController alloc] initWithLatitude:52.519171 withLongitude:13.4060912 withCountryCode:@"DE" withCity:@"Berlin"];
     [self.navigationController pushViewController:vc animated:YES];
     
-    int max = 5;
-    for(int i=1;i<=max;i++)
-    {
-            [self.view addSubview:[self viewWithDelay:i*duration/(CGFloat)max]];
-    }
-
-}
-
-- (UIView*)viewWithDelay:(CGFloat)delay
-{
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(200, 200, 100, 100)];
-    view.backgroundColor = [UIColor clearColor];
-    view.layer.cornerRadius = 50;
-    view.layer.borderColor = [UIColor orangeColor].CGColor;
-    view.layer.borderWidth = 1;
-
-    view.center = CGPointMake(CGRectGetWidth(self.view.frame)/2.0, CGRectGetHeight(self.view.frame)/2.0);
-    view.hidden = YES;
-    [self performSelector:@selector(animateView:) withObject:view afterDelay:delay];
-    return view;
-}
-
-- (void)animateView:(UIView*)view
-{
-    view.hidden = NO;
-    CABasicAnimation *scaleAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-    scaleAnimation.duration = duration;
-    scaleAnimation.repeatCount = HUGE_VAL;
-    scaleAnimation.autoreverses = NO;
-    scaleAnimation.fromValue = [NSNumber numberWithFloat:0.1];
-    scaleAnimation.toValue = [NSNumber numberWithFloat:6.5];
     
-    [view.layer addAnimation:scaleAnimation forKey:@"scale"];
+
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
